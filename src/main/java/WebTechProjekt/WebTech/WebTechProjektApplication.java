f31beb7095d677a2bcd357e726103cfbc86b5fda
KurtogluSinem <krtgl.sinem2519@gmail.com>
2022-10-12 14:31:01 +0200

    1.schritt

---
+++ src/main/java/WebTechProjekt/WebTech/WebTechProjektApplication.java
@@ -0,0 +1,13 @@
+package WebTechProjekt.WebTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WebTechProjekt/WebTech/WebTechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package WebTechProjekt.WebTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
